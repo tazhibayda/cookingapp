cc42fa2483311003589aa33e262b71bced012622
tazhibayda <200103210@stu.sdu.edu.kz>
2022-10-22 00:53:40 +0600

    clickable button but in hard way need to change with fragment

--- app/src/main/java/com/example/cookingapp/HomePageActivity.kt
+++ app/src/main/java/com/example/cookingapp/HomePageActivity.kt
@@ -6,18 +6,23 @@ import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
+import com.example.cookingapp.databinding.ActivityMainBinding
 import com.example.cookingapp.databinding.HomePageBinding
 
 class HomePageActivity : AppCompatActivity() {
 
-    private lateinit var binding: HomePageBinding
+    private lateinit var homePagebinding: HomePageBinding
+    private lateinit var activitymainbinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        binding = HomePageBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        homePagebinding = HomePageBinding.inflate(layoutInflater)
+        activitymainbinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activitymainbinding.root)
+        activitymainbinding.start.setOnClickListener {
+            setContentView(homePagebinding.root)
+        }
 
     }
 }
