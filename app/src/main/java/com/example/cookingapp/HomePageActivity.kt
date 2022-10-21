4aadc5c9590e53744cd69084b2841832db6e090d
tazhibayda <91252221+nurik17@users.noreply.github.com>
2022-10-22 00:13:53 +0600

    Button Outlined

--- app/src/main/java/com/example/cookingapp/HomePageActivity.kt
+++ app/src/main/java/com/example/cookingapp/HomePageActivity.kt
@@ -1,7 +1,11 @@
 package com.example.cookingapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import com.example.cookingapp.databinding.HomePageBinding
 
 class HomePageActivity : AppCompatActivity() {
