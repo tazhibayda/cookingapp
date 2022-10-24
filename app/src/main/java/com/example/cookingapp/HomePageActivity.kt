fa9f16286e154ff51aefa278c759314f018cc895
tazhibayda <200103210@stu.sdu.edu.kz>
2022-10-24 10:20:31 +0600

    Bottom bar works correctly, added fragments

--- app/src/main/java/com/example/cookingapp/HomePageActivity.kt
+++ app/src/main/java/com/example/cookingapp/HomePageActivity.kt
@@ -3,11 +3,14 @@ package com.example.cookingapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.replace
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.cookingapp.adapters.RecipeAdapter
 import com.example.cookingapp.databinding.ActivityMainBinding
+import com.example.cookingapp.databinding.FragmentRecipesBinding
 import com.example.cookingapp.databinding.HomePageBinding
 import com.example.cookingapp.databinding.MyRecipesBinding
 import com.example.cookingapp.models.RecipeModel
@@ -18,25 +21,39 @@ class HomePageActivity : AppCompatActivity() {
     private lateinit var homePagebinding: HomePageBinding
     private lateinit var activitymainbinding: ActivityMainBinding
     private lateinit var myRecipesBinding: MyRecipesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
+
         homePagebinding = HomePageBinding.inflate(layoutInflater)
         activitymainbinding = ActivityMainBinding.inflate(layoutInflater)
         myRecipesBinding = MyRecipesBinding.inflate(layoutInflater)
-        setContentView(activitymainbinding.root)
+
+        setContentView(homePagebinding.root)
+
         activitymainbinding.start.setOnClickListener {
             setContentView(homePagebinding.root)
         }
 
+        bottomBarNavigation()
 
-        homePagebinding.bottomNavigatinView.setOnNavigationItemSelectedListener { item ->
+
+
+
+
+    }
+
+
+    private fun bottomBarNavigation(){
+        homePagebinding.bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             when(item.itemId) {
                 R.id.inspirations -> {
+                    replaceFragment(InspirationFragment())
                     true
                 }
                 R.id.myrecipes -> {
-                    setContentView(myRecipesBinding.root)
+                    replaceFragment(RecipesFragment())
                     true
                 }
                 R.id.cooknow -> {
@@ -48,45 +65,12 @@ class HomePageActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
 
-        myRecipesBinding.bottomNavigatinView.setOnNavigationItemSelectedListener { item ->
-            when(item.itemId) {
-                R.id.inspirations -> {
-                    setContentView(homePagebinding.root)
-                    true
-                }
-                R.id.myrecipes -> {
-                    setContentView(myRecipesBinding.root)
-                    true
-                }
-                R.id.cooknow -> {
-                    true
-                }
-                R.id.settings -> {
-                    true
-                }
-                else -> false
-            }
-        }
-
-
-
-        val recieps = myRecipesBinding.myRecipes
-        val reciepList: ArrayList<RecipeModel> = ArrayList<RecipeModel>()
-        reciepList.add(RecipeModel("DSA in Java", "asdhiasdasdbashd",45,3, R.drawable.chef))
-        reciepList.add(RecipeModel("DSA in Java", "asdhiasdasdbashd",45,3, R.drawable.chef))
-        reciepList.add(RecipeModel("What is Lorem Ipsum?",
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",45,3, R.drawable.chef))
-        reciepList.add(RecipeModel("DSA in Java", "asdhiasdasdbashd",45,3, R.drawable.chef))
-        reciepList.add(RecipeModel("DSA in Java", "asdhiasdasdbashd",45,3, R.drawable.chef))
-        reciepList.add(RecipeModel("DSA in Java", "asdhiasdasdbashd",45,3, R.drawable.chef))
-        reciepList.add(RecipeModel("DSA in Java ajkn", "aasdassdhiasdasdbashd",90,6, R.drawable.dishes))
-
-        val recipeAdapter = RecipeAdapter(this, reciepList)
-
-        val layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-
-        recieps.layoutManager = layoutManager
-        recieps.adapter = recipeAdapter
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(homePagebinding.fragmentContainerView.id,fragment)
+        fragmentTransaction.commit()
     }
 }
---
+++ app/src/main/java/com/example/cookingapp/RecipesFragment.kt
@@ -0,0 +1,59 @@
+package com.example.cookingapp
+
+import android.content.Intent.getIntent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.cookingapp.adapters.RecipeAdapter
+import com.example.cookingapp.models.RecipeModel
+
+
+class RecipesFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        val view =  inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        val recieps:RecyclerView = view.findViewById(R.id.my_recipes)
+
+        val reciepList: ArrayList<RecipeModel> = ArrayList<RecipeModel>()
+        reciepList.add(RecipeModel("DSA in Java", "asdhiasdasdbashd",45,3, R.drawable.chef))
+        reciepList.add(RecipeModel("DSA in Java", "asdhiasdasdbashd",45,3, R.drawable.chef))
+        reciepList.add(RecipeModel("What is Lorem Ipsum?",
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",45,3, R.drawable.chef))
+        reciepList.add(RecipeModel("DSA in Java", "asdhiasdasdbashd",45,3, R.drawable.chef))
+        reciepList.add(RecipeModel("DSA in Java", "asdhiasdasdbashd",45,3, R.drawable.chef))
+        reciepList.add(RecipeModel("DSA in Java", "asdhiasdasdbashd",45,3, R.drawable.chef))
+        reciepList.add(RecipeModel("DSA in Java ajkn", "aasdassdhiasdasdbashd",90,6, R.drawable.dishes))
+
+        val recipeAdapter = RecipeAdapter(HomePageActivity(), reciepList)
+        val layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        recieps.layoutManager = layoutManager
+        recieps.adapter = recipeAdapter
+
+
+        return view
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            RecipesFragment().apply {
+
+            }
+    }
+}
