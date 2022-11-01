f4615ea0a2ad7409a2f2887787d944ddf5e31d7a
tazhibayda <200103210@stu.sdu.edu.kz>
2022-11-01 16:47:11 +0600

    Bottom bar, Setting fixed

---
+++ app/src/main/java/com/example/cookingapp/SettingsFragment.kt
@@ -0,0 +1,14 @@
+package com.example.cookingapp
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+    }
+
+}
